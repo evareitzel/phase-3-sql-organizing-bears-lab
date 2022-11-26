bcb347eda87b4df309cb3e2ca6fb68e99cb1af9d
Eva Reitzel <evareitzel@gmail.com>
2022-11-26 13:32:33 -0500

    commit tree 5cfce6d1d5ae89d76e82e8443f40228cf4d92089

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 12, 'M', 'brown', 'sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 12, 'M', 'brown', 'gentle', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 4, 'F', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 8, 'M', 'brown', 'gentle', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 1, 'F', 'brown', 'gentle', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 32, 'M', 'brown', 'gentle', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 4, 'F', 'rainbow sparkly', 'difficult', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 3, 'M', 'brown', 'gentle', 1);
